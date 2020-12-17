f5337ec224bebf98b8ff7b01d7531fb82978f897
Berkay <berkay.dincer@craftbase.io>
2020-12-17 15:23:07 +0300

    adds checkout payment (#4)

---
+++ samples/payment/RetrieveCheckoutPayment.js
@@ -0,0 +1,11 @@
+const TokenPay = require('../../dist');
+
+const tokenPay = new TokenPay.Client({
+  apiKey: 'api-key',
+  secretKey: 'secret-key',
+  baseUrl: 'https://api-gateway.tokenpay.com.tr'
+});
+
+tokenPay.payment().retrieveCheckoutPayment('fe4b0c2d-3c48-4553-9429-695d204bd7c1')
+  .then(result => console.info('retrieve checkout payment successful', result))
+  .catch(err => console.error('Failed to retrieve checkout payment', err));
